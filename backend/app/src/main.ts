05f2dd9760063242b3523aff18d8191107010f22
n622 <jwithelis@gmail.com>
2025-02-18 19:10:14 +0900

    CROS設定を環境変数に隔離

--- backend/app/src/main.ts
+++ backend/app/src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // ブラウザのローカルホストからのアクセスを許可する
   app.enableCors({
-    origin: '*', // Next.jsの開発サーバーのURL (例)
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    origin: process.env.CORS_ORIGIN,
+    methods: process.env.CORS_METHODS,
+    allowedHeaders: process.env.CORS_ALLOWED_HEADERS,
   });
-  await app.listen(process.env.PORT ?? 8000);
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
